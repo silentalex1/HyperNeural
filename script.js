5a393ce061297184b320fb1f3bf5ea081a8d4fa8
Zal <101911964+silentalex1@users.noreply.github.com>
2026-06-20 22:36:30 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('mousemove', e => {
-    const bg = document.getElementById('glow-bg');
-    if (bg) {
-        bg.style.transform = `translate(${e.clientX * 0.04}px, ${e.clientY * 0.04}px)`;
-    }
-});
