69ebf9c362797aa4a43ae6785347453b392a2380
Zal <101911964+silentalex1@users.noreply.github.com>
2026-07-02 00:13:02 -0400

    Delete hyper neural training directory

--- hyper neural training/hyperneural_turbo/core/__init__.py
+++
@@ -1,4 +0,0 @@
-from .trainer import HyperTrainer, TrainingConfig, SyntheticDataGenerator
-from .advanced_trainer import AdvancedHyperTrainer, AdvancedTrainingConfig, AdvancedDataProcessor
-
-__all__ = ["HyperTrainer", "TrainingConfig", "SyntheticDataGenerator", "AdvancedHyperTrainer", "AdvancedTrainingConfig", "AdvancedDataProcessor"]
